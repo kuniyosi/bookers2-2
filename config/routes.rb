fcc699890d95de21863b7f911aaf088c6fec9c35
nakazawa <zhongzejiangji@gmail.com>
2023-04-29 20:35:40 +0000

    views add

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  get 'homes/top'
-  get 'homes/about'
+
   devise_for :users
- 
+
+  resources :users, only:[:index, :show, :edit]
+  resources :books, only:[:index, :show, :edit]
+
+  root to: 'homes#top'
+  get 'about' => 'homes#about', as: 'about'
+
+
 end
